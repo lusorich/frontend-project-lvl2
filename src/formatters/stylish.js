3843335e567a625703a2814b12c089f219a0dff5
dreamonaut <dreamonautre@gmail.com>
2022-06-18 17:22:24 +0500

    Adding stylish formatter

---
+++ src/formatters/stylish.js
@@ -0,0 +1,71 @@
+const spacesCount = 1;
+const replacer = '    ';
+
+const getCurrentIndentsByType = (type, repeatCount) => {
+  const currentIndent = replacer.repeat(repeatCount);
+  const countReplacedChar = 2;
+  if (type) {
+    switch (type) {
+      case 'add':
+        return [
+          `${currentIndent.slice(0, currentIndent.length - countReplacedChar)
+          }+`
+            + ' ',
+        ];
+      case 'miss':
+        return [
+          `${currentIndent.slice(0, currentIndent.length - countReplacedChar)
+          }-`
+            + ' ',
+        ];
+      case 'diff': {
+        return [
+          `${currentIndent.slice(0, currentIndent.length - countReplacedChar)
+          }+`
+            + ' ',
+          `${currentIndent.slice(0, currentIndent.length - countReplacedChar)
+          }-`
+            + ' ',
+        ];
+      }
+      default:
+        return [currentIndent];
+    }
+  }
+  return [currentIndent];
+};
+
+const stylish = (obj) => {
+  const iter = (currentValue, depth) => {
+    if (typeof currentValue !== 'object' || currentValue === null) {
+      return `${currentValue}`;
+    }
+
+    const indentSize = depth * spacesCount;
+    const bracketIndent = replacer.repeat(indentSize - spacesCount);
+    const lines = Object.keys(currentValue).map((key) => {
+      const { type, value } = currentValue[key];
+      const [currentIndent1, currentIndent2] = getCurrentIndentsByType(
+        type,
+        indentSize,
+      );
+      if (Array.isArray(value)) {
+        const [value1, value2] = value;
+        return `${currentIndent1}${key}: ${iter(
+          value1,
+          depth + 1,
+        )}\n${currentIndent2}${key}: ${iter(value2, depth + 1)}`;
+      }
+      return `${currentIndent1}${key}: ${iter(
+        value || currentValue[key],
+        depth + 1,
+      )}`;
+    });
+
+    return ['{', ...lines, `${bracketIndent}}`].join('\n');
+  };
+
+  return iter(obj, 1);
+};
+
+export { getCurrentIndentsByType, stylish };
--- src/functions.js
+++ src/functions.js
@@ -1,6 +1,7 @@
 import * as fs from 'node:fs';
 import _ from 'lodash';
 import buildTree from './builder.js';
+import { stylish } from './formatters/stylish.js';
 
 const hasKey = (obj, key) => Object.prototype.hasOwnProperty.call(obj, key);
 const isFileExists = (...paths) => paths.filter((p) => fs.existsSync(p)).length === paths.length;
@@ -56,13 +57,17 @@ const compare = (obj1, obj2) => {
   }, {});
 };
 
-function readAndCompareFiles() {
-  const [path1, path2] = this.args;
+function readAndCompareFiles(path1, path2, formatter) {
   if (isFileExists(path1, path2)) {
     const [obj1, obj2] = buildTree(path1, path2);
-    const heh = compare(obj1, obj2);
-    console.log('heh', heh);
-    return heh;
+    const diffTree = compare(obj1, obj2);
+    switch (formatter) {
+      case 'stylish': {
+        console.log(stylish(diffTree));
+        return stylish(diffTree);
+      }
+      default: return '';
+    }
   }
   return {};
 }
