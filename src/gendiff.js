7b694e9f9a98aa9d8b34512b69a1c164dc38787f
dreamonaut <dreamonautre@gmail.com>
2022-06-19 23:38:10 +0500

    Argv adding

--- src/gendiff.js
+++ src/gendiff.js
@@ -5,7 +5,7 @@ import { compare } from './functions.js';
 
 const isFileExists = (...paths) => paths.filter((p) => fs.existsSync(p)).length === paths.length;
 
-export default (path1, path2, formatter) => {
+export default (path1, path2, formatter = 'stylish') => {
   if (isFileExists(path1, path2)) {
     const [obj1, obj2] = buildTree(path1, path2);
     const diffTree = compare(obj1, obj2);
