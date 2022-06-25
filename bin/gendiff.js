33e221723ecda9780a1f7d4997f522a49cca4031
dreamonaut <dreamonautre@gmail.com>
2022-06-25 21:15:21 +0500

    delete modern imports

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,10 +9,8 @@ program
   .argument('<filepath1>')
   .argument('<filepath2>')
   .option('-f, --format <type>', 'output format', 'stylish')
-  .action((args) => {
-    const [path1, path2] = [args[0], args[1]];
-    const formatter = args[2].format;
-    console.log(getDiff(path1, path2, formatter));
+  .action((path1, path2, options) => {
+    console.log(getDiff(path1, path2, options.format));
   });
 
 program.parse(process.argv);
