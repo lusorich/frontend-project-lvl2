84c7adedd0c360b016163b1089cbc81651cc4a85
dreamonaut <dreamonautre@gmail.com>
2022-06-25 21:16:20 +0500

    delete modern imports

--- src/builder.js
+++ src/builder.js
@@ -3,7 +3,6 @@ import { extname } from 'path';
 import parser from './parsers.js';
 
 export default (path1, path2) => {
-  console.log("path1", path1);
   const file1 = readFileSync(path1);
   const file2 = readFileSync(path2);
   return [
